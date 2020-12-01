58f522bb4be97243f86c098e418c94c6ae7f089a
Paul Caruana <paul.caruana@leighton.com>
2020-12-01 09:58:06 +0000

    Introduction of Radium for styling media queries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -49,7 +50,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding:'8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -69,19 +74,34 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
+    let classes = [];
+
+    if (this.state.persons.length <= 2){
+      classes.push('red'); // classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold'); // classes = ['red', 'bold']
+    }
+
+
     return (
+      <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button 
         style={style}
         onClick={this.togglePersonsHandler}>Toggle People</button>
         {persons}
       </div>
-
+      </StyleRoot>
     )};
   }
 
-export default App;
+export default Radium(App);
