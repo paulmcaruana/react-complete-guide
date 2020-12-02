68b404a29b52589064b3f117b4f12652fb24bb4a
Paul Caruana <paul.caruana@leighton.com>
2020-12-02 14:38:55 +0000

    Split into components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,17 +3,9 @@ import React from 'react';
 import classes from './Person.css';
 
 const person = (props) => {    
-    const rnd = Math.random();
-
-    if (rnd > 0.7) {
-        throw new Error ('Someting went wrong');        
-    }
-
     return (
    <div className={classes.Person}>
-    <p onClick={props.click}>
-    I'm {props.name} and I am {props.age} years old!
-    </p>
+    <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
     <p>{props.children}</p>
     <input type="text" onChange={props.changed} value={props.name}/>
     </div>
